f049ba0a82e43997eb19df2aa5f6bf4a007000ca
Maycon Leite <leite.maycon@live.com>
2021-02-23 16:06:18 -0300

    chore: add sqlite config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
-import express, { response } from 'express';
+import 'reflect-metadata';
+import express from 'express';
+import "./database";
 
 const app = express();
+
 
 app.get("/", (request, response) => {
     return response.json({message: "hellow world NLW4!"})
