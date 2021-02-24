a161f6aa1486d055fcf8b0376b782a89ab7206ce
Maycon Leite <leite.maycon@live.com>
2021-02-24 03:10:42 -0300

    chore: add validate users

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -7,6 +7,16 @@ class UserController {
         const { name, email } = request.body;
         
         const usersRepository = getRepository(User);
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if(userAlreadyExists) {
+            return response.status(400).json({
+                error: "User already exists!",
+            })
+        }
         
         const user = usersRepository.create({
             name,
